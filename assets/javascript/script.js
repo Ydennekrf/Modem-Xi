76aa11747cafc4907a4a9369e7cc7109f75cd5b4
ydennekrf <ydennekrf@gmail.com>
2022-05-26 08:31:52 -0400

    set up local storage to save on clicking the calander icon

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,8 @@
+// adds current date by day word month day
 $('#currentDay').text(moment().format("dddd MMMM Do"));
+
+//saves text conent to loacl storeage
+$('.saveBtn').on('click', function () {
+    localStorage.setItem($(this).prev().attr("id"), $(this).prev().val());
+});
+
