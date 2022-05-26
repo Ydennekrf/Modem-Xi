6483baabb0459ec393c53b75993b56c835ce8b19
ydennekrf <ydennekrf@gmail.com>
2022-05-26 08:47:47 -0400

    added Jquery function for get local storage and to alter background color based on time will have to test out between 9 and 5pm

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,16 @@ $('.saveBtn').on('click', function () {
     localStorage.setItem($(this).prev().attr("id"), $(this).prev().val());
 });
 
+// changes background color based on current time will have to test during time between 9-5.
+//gets info from local storage and renders it in the appropriate cell.
+$('input').each(function () {
+    $(this).val(localStorage.getItem($(this).attr("id")))
+    if (parseInt($(this).attr("id")) > moment().hour()) {
+        $(this).addClass("future")
+    } else if (parseInt($(this).attr("id")) < moment().hour()) {
+        $(this).addClass('past')
+    } else {
+        $(this).addClass('present')
+    }
+});
+
